c85b41144f4148a58dbc0096898ff366788abc5a
akray <akray0413@gmail.com>
2023-01-28 17:07:38 +0530

    amit

---
+++ src/test/java/gitPractice/Practice.java
@@ -0,0 +1,16 @@
+package gitPractice;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void hello()
+	{
+		System.out.println("practice was executed");
+	}
+	@Test
+	public void hello2()
+	{
+		System.out.println("practice1 was executed");
+	}
+}
